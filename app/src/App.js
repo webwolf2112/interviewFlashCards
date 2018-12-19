8689333d112d48d9d671c182d9faf7d2c32a63bb
Vanessa Henson <vanessahenson@Vanessas-Air.local>
2018-12-18 20:11:04 -0700

    installed create-react-app

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to Interview Flash Cards</h1>
+        <p>This is here to help anyone who wants to remember all of those tricky interview quesions.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
